682e128b11198c3ba915ae99713ab372537848af
pabrcno <pabrcn@gmail.com>
2023-05-29 14:59:42 -0300

    git checkout main

---
+++ lib/domain/models/user/app_user.dart
@@ -0,0 +1,27 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'app_user.freezed.dart';
+part 'app_user.g.dart';
+
+enum ESubscriptions {
+  Basic,
+  Premium,
+  VIP
+} // replace with your actual subscriptions enum
+
+@freezed
+class AppUser with _$AppUser {
+  const factory AppUser({
+    required String uid,
+    required String email,
+    required String displayName,
+    required String photoURL,
+    required bool emailVerified,
+    required ESubscriptions subscription,
+    required int messageCount,
+    required DateTime createdAt,
+  }) = _AppUser;
+
+  factory AppUser.fromJson(Map<String, dynamic> json) =>
+      _$AppUserFromJson(json);
+}
