ab52b1a15408824aa3937020b52d807481982cf1
pabrcno <pabrcn@gmail.com>
2023-06-19 14:38:30 -0300

    Fix ux disabled options

--- lib/presentation/chat/widgets/tools/prompt_builder.dart
+++ lib/presentation/chat/widgets/tools/prompt_builder.dart
@@ -6,6 +6,7 @@ import 'package:flutter_translate/flutter_translate.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:topics/app/chat/chat_provider.dart';
+import 'package:topics/presentation/widgets/disabled.dart';
 
 class PromptBuilderTools extends StatefulWidget {
   PromptBuilderTools({Key? key}) : super(key: key);
@@ -79,73 +80,86 @@ class _PromptBuilderToolsState extends State<PromptBuilderTools> {
                             ),
                           ));
                     },
-                    body: SizedBox(
-                      height: 180,
-                      child: ListView.builder(
-                        itemCount: data.entries.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          var entry = data.entries.elementAt(index);
-                          return Padding(
-                            padding: const EdgeInsets.only(bottom: 12),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 12),
-                                  child: Text(
-                                    translate(entry.key),
-                                    style:
-                                        Theme.of(context).textTheme.bodyMedium,
-                                  ),
-                                ),
-                                const SizedBox(height: 8),
-                                SizedBox(
-                                  height: 35,
-                                  width: MediaQuery.of(context).size.width,
-                                  child: ListView.separated(
-                                    separatorBuilder:
-                                        (BuildContext context, int i) {
-                                      return const SizedBox(width: 12);
-                                    },
-                                    scrollDirection: Axis.horizontal,
-                                    itemCount: entry.value.length,
-                                    itemBuilder: (BuildContext context, int i) {
-                                      return Padding(
-                                        padding: const EdgeInsets.all(5.0),
-                                        child: ElevatedButton(
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor: getColor(index),
-                                            padding: const EdgeInsets.symmetric(
-                                              horizontal: 12,
+                    body: Disabled(
+                        disabled: Provider.of<ChatProvider>(context,
+                                    listen: false)
+                                .isLoading ||
+                            Provider.of<ChatProvider>(context, listen: false)
+                                .messageBuffer
+                                .isNotEmpty,
+                        child: SizedBox(
+                          height: 180,
+                          child: ListView.builder(
+                            itemCount: data.entries.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              var entry = data.entries.elementAt(index);
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 12),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 12),
+                                      child: Text(
+                                        translate(entry.key),
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyMedium,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    SizedBox(
+                                      height: 35,
+                                      width: MediaQuery.of(context).size.width,
+                                      child: ListView.separated(
+                                        separatorBuilder:
+                                            (BuildContext context, int i) {
+                                          return const SizedBox(width: 12);
+                                        },
+                                        scrollDirection: Axis.horizontal,
+                                        itemCount: entry.value.length,
+                                        itemBuilder:
+                                            (BuildContext context, int i) {
+                                          return Padding(
+                                            padding: const EdgeInsets.all(5.0),
+                                            child: ElevatedButton(
+                                              style: ElevatedButton.styleFrom(
+                                                backgroundColor:
+                                                    getColor(index),
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                  horizontal: 12,
+                                                ),
+                                              ),
+                                              onPressed: () {
+                                                Provider.of<ChatProvider>(
+                                                        context,
+                                                        listen: false)
+                                                    .sendMessage(entry.value[i]
+                                                        ["instructions"]);
+                                              },
+                                              child: Text(
+                                                translate(entry.value[i]
+                                                        ["title"] ??
+                                                    ''),
+                                                style: TextStyle(
+                                                  color: Theme.of(context)
+                                                      .colorScheme
+                                                      .surface,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
                                             ),
-                                          ),
-                                          onPressed: () {
-                                            Provider.of<ChatProvider>(context,
-                                                    listen: false)
-                                                .sendMessage(entry.value[i]
-                                                    ["instructions"]);
-                                          },
-                                          child: Text(
-                                            translate(
-                                                entry.value[i]["title"] ?? ''),
-                                            style: TextStyle(
-                                              color: Theme.of(context)
-                                                  .colorScheme
-                                                  .surface,
-                                              fontSize: 12,
-                                            ),
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                    ),
+                              );
+                            },
+                          ),
+                        )),
                     isExpanded: _isVisible,
                   ),
                 ],
